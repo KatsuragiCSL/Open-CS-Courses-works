41056108366fa25cd7c089007bb170ef25adb0a7
KatsuragiCSL <shue12345678@gmail.com>
2022-05-06 14:03:11 +0800

    CS61B readme

---
+++ UCB CS61B/skeleton-sp21/lab1/Collatz.java
@@ -0,0 +1,25 @@
+/** Class that prints the Collatz sequence starting from a given number.
+ *  @author KatsuragiCSL
+ */
+public class Collatz {
+
+    /** Buggy implementation of nextNumber! */
+    public static int nextNumber(int n) {
+        if (n  % 2 == 0) {
+            return n / 2;
+        } else {
+            return 3 * n + 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.print(n + " ");
+        while (n != 1) {
+            n = nextNumber(n);
+            System.out.print(n + " ");
+        }
+        System.out.println();
+    }
+}
+
