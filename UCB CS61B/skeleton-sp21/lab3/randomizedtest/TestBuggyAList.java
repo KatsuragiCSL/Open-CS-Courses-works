012e40f5c0aaf13a3670e74db2c2fbbe5da61b95
KatsuragiCSL <shue12345678@gmail.com>
2022-05-16 03:14:19 +0800

    CS61B lab3 forth task

--- UCB CS61B/skeleton-sp21/lab3/randomizedtest/TestBuggyAList.java
+++ UCB CS61B/skeleton-sp21/lab3/randomizedtest/TestBuggyAList.java
@@ -9,4 +9,22 @@ import static org.junit.Assert.*;
  */
 public class TestBuggyAList {
   // YOUR TESTS HERE
+    @Test
+    /** adds the same value to both the AlistNoResizing and buggy AList implementations,
+     *  then checks that the results of three subsequent removeLast calls are the same */
+    public void testThreeAddThreeRemove() {
+      AListNoResizing<Integer> A = new AListNoResizing<Integer>();
+      BuggyAList<Integer> B = new BuggyAList<Integer>();
+
+      A.addLast(4);
+      B.addLast(4);
+      A.addLast(5);
+      B.addLast(5);
+      A.addLast(6);
+      B.addLast(6);
+
+      assertEquals(A.removeLast(), B.removeLast());
+      assertEquals(A.removeLast(), B.removeLast());
+      assertEquals(A.removeLast(), B.removeLast());
+    }
 }
