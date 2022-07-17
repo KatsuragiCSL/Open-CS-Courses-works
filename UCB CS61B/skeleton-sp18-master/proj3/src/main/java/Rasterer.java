d33482e8582bccec374bddb02fe6fc695924d10f
KatsuragiCSL <shue12345678@gmail.com>
2022-07-18 03:42:01 +0800

    cs61b proj3

--- UCB CS61B/skeleton-sp18-master/proj3/src/main/java/Rasterer.java
+++ UCB CS61B/skeleton-sp18-master/proj3/src/main/java/Rasterer.java
@@ -9,6 +9,8 @@ import java.util.Map;
  */
 public class Rasterer {
 
+    private static final double ROOT_ULLAT = MapServer.ROOT_ULLAT, ROOT_ULLON = MapServer.ROOT_ULLON, ROOT_LRLAT = MapServer.ROOT_LRLAT, ROOT_LRLON = MapServer.ROOT_LRLON;
+    private static final int TILE_SIZE = MapServer.TILE_SIZE;
     public Rasterer() {
         // YOUR CODE HERE
     }
@@ -42,11 +44,88 @@ public class Rasterer {
      *                    forget to set this to true on success! <br>
      */
     public Map<String, Object> getMapRaster(Map<String, Double> params) {
+
         // System.out.println(params);
         Map<String, Object> results = new HashMap<>();
-        System.out.println("Since you haven't implemented getMapRaster, nothing is displayed in "
-                           + "your browser.");
+        /* System.out.println("Since you haven't implemented getMapRaster, nothing is displayed in "
+                           + "your browser."); */
+        double ullat = params.get("ullat");
+        double ullon = params.get("ullon");
+        double lrlat = params.get("lrlat");
+        double lrlon = params.get("lrlon");
+        double width = params.get("w");
+        double height = params.get("h");
+        int depth = computeDepth(lrlon - ullon, width);
+        boolean query_success = true;
+
+        //boundary checks
+        if (ullat < lrlat || lrlon < ullon || ROOT_LRLON <= ullon || lrlon <= ROOT_ULLON || ullat <= ROOT_LRLAT || ROOT_ULLAT <= lrlat ) {
+            query_success = false;
+        }
+
+        double widthPerPicture = (ROOT_LRLON - ROOT_ULLON) / Math.pow(2.0, depth);
+        double heightPerPicture = (ROOT_ULLAT - ROOT_LRLAT) / Math.pow(2.0, depth);
+        // # of pic at the boundary
+        int xMin = (int) Math.floor((ullon - ROOT_ULLON) / widthPerPicture);
+        int xMax = (int) Math.floor((lrlon - ROOT_ULLON) / widthPerPicture);
+        int yMin = (int) Math.floor((ROOT_ULLAT - ullat) / heightPerPicture);
+        int yMax = (int) Math.floor((ROOT_ULLAT - lrlat) / heightPerPicture);
+        // boundary of the pic
+        double xMinBound = ROOT_ULLON + xMin * widthPerPicture;
+        double xMaxBound = ROOT_ULLON + (xMax + 1) * widthPerPicture;
+        double yMinBound = ROOT_ULLAT - yMin * heightPerPicture;
+        double yMaxBound = ROOT_ULLAT - (yMax + 1) * heightPerPicture;
+
+        // edge cases
+        if (ullon < ROOT_ULLON) {
+            xMinBound = ROOT_ULLON;
+            xMin = 0;
+        }
+        if (lrlon > ROOT_LRLON) {
+            xMax = (int)Math.pow(2, depth) - 1;
+            xMaxBound = ROOT_LRLON;
+        }
+        if (ullat > ROOT_ULLAT) {
+            yMin = 0;
+            yMinBound = ROOT_ULLAT;
+        }
+        if (lrlat < ROOT_LRLAT) {
+            yMax = (int)Math.pow(2, depth) - 1;
+            yMaxBound = ROOT_LRLAT;
+        }
+
+        // grids matrix
+        String[][] render_grid = new String[yMax - yMin + 1][xMax - xMin + 1];
+        for (int i = yMin; i <= yMax; i++) {
+            for (int j = xMin; j <= xMax; j++) {
+                render_grid[i - yMin][j - xMin] = "d" + depth + "_x" + j + "_y" + i + ".png";
+            }
+        }
+
+        results.put("render_grid", render_grid);
+        results.put("raster_ul_lon", xMinBound);
+        results.put("raster_lr_lon", xMaxBound);
+        results.put("raster_ul_lat", yMinBound);
+        results.put("raster_lr_lat", yMaxBound);
+        results.put("depth", depth);
+        results.put("query_success", query_success);
+
         return results;
+    }
+
+    /**
+     * Takes the difference in longitude and the width of the query area and return the depth.
+     *
+     * @param lonDiff difference of longitudes.
+     * @param w width of the query area.
+     *
+     * @return An int that indicates the depth of the images.
+     */
+    private int computeDepth(double lonDiff, double w) {
+        double lonDPP = lonDiff / w;
+        double n = Math.log((ROOT_LRLON - ROOT_ULLON) / (lonDPP * TILE_SIZE)) / Math.log(2.0);
+        int ret = (int)Math.ceil(n);
+        return ret >= 7 ? 7 : ret;
     }
 
 }
