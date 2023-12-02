c52b5c0d81b268f76f8132d4e3a257bd0650bb62
KatsuragiCSL <shue12345678@gmail.com>
2023-12-02 23:05:57 +0800

    Stanford CS110l assignment 1 done

--- Stanford CS110L/cs110l-spr-2020-starter-code-main/week1/part-3-hangman/src/main.rs
+++ Stanford CS110L/cs110l-spr-2020-starter-code-main/week1/part-3-hangman/src/main.rs
@@ -17,6 +17,7 @@ use rand::Rng;
 use std::fs;
 use std::io;
 use std::io::Write;
+use std::collections::HashSet;
 
 const NUM_INCORRECT_GUESSES: u32 = 5;
 const WORDS_PATH: &str = "words.txt";
@@ -27,6 +28,79 @@ fn pick_a_random_word() -> String {
     String::from(words[rand::thread_rng().gen_range(0, words.len())].trim())
 }
 
+fn print_guess_left(guess_left: u32) {
+	println!("You have {} guesses left", guess_left);
+}
+
+fn print_guessed_words(guessed_words: &HashSet<char>) {
+	println!("You have guessed the following letters:");
+	for c in guessed_words {
+		print!("{}", c);
+	}
+	println!("");
+}
+
+fn print_revealed_words(revealed_words: &Vec<char>) {
+	print!("The word so far is ");
+	for c in revealed_words {
+		print!("{}", c);
+	}
+	println!("");
+}
+
+fn game_loop(secret_word: &String, mut chance: u32) {
+		let secret_word_chars: Vec<char> = secret_word.chars().collect();
+		let mut revealed_words =  Vec::new();
+		let mut guessed_words = HashSet::new();
+		let mut not_guessed_words = HashSet::new();
+		for i in 0..secret_word_chars.len() {
+			revealed_words.push('_');
+			not_guessed_words.insert(secret_word_chars[i]);
+		}
+		loop {
+			print_revealed_words(&revealed_words);
+
+			print_guessed_words(&guessed_words);
+
+			print_guess_left(chance);
+
+			print!("Please guess a letter: ");
+			// Make sure the prompt from the previous line gets displayed:
+			io::stdout()
+				.flush()
+				.expect("Error flushing stdout.");
+			let mut guess = String::new();
+			io::stdin()
+				.read_line(&mut guess)
+				.expect("Error reading line.");
+
+			//preventing more than 1 chars in input
+			let tmp: Vec<char> = guess.chars().collect();
+			let guess_char = tmp[0];
+			if (not_guessed_words.contains(&guess_char)) {
+				not_guessed_words.remove(&guess_char);
+				guessed_words.insert(guess_char);
+				for i in 0..secret_word_chars.len() {
+					if secret_word_chars[i] == guess_char {
+						revealed_words[i] = guess_char;
+					}
+				}
+			} else {
+				chance -= 1;
+				println!("Sorry, that letter is not in the word");
+			}
+
+			if (chance == 0) {
+				println!("Sorry, you ran out of guesses!");
+				break;
+			}
+			if (not_guessed_words.is_empty()) {
+				println!("Congratulations you guessed the secret word: {}", secret_word);
+				break;
+			}
+	}
+}
+
 fn main() {
     let secret_word = pick_a_random_word();
     // Note: given what you know about Rust so far, it's easier to pull characters out of a
@@ -34,7 +108,11 @@ fn main() {
     // secret_word by doing secret_word_chars[i].
     let secret_word_chars: Vec<char> = secret_word.chars().collect();
     // Uncomment for debugging:
-    // println!("random word: {}", secret_word);
+     println!("random word: {}", secret_word);
 
     // Your code here! :)
+	let mut chance: u32 = NUM_INCORRECT_GUESSES;
+
+	println!("Welcome to CS110L Hangman!");
+	game_loop(&secret_word, chance);
 }
