27bce9929eb9af63083b774b69cc5917fee244f8
KatsuragiCSL <shue12345678@gmail.com>
2023-12-17 02:18:18 +0800

    Stanford CS110L week2 rwc done

--- Stanford CS110L/cs110l-spr-2020-starter-code-main/week2/rwc/src/main.rs
+++ Stanford CS110L/cs110l-spr-2020-starter-code-main/week2/rwc/src/main.rs
@@ -1,7 +1,33 @@
 use std::env;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::io::Read;
 use std::process;
 
+fn count_characters(filename: &String) -> Result<usize, io::Error> {
+	let file = File::open(filename)?;
+	let mut buffer = Vec::new();
+	let ret = io::BufReader::new(file).read_to_end(&mut buffer)?;
+	Ok(ret)
+}
 
+fn count_words(filename: &String) -> Result<usize, io::Error> {
+	let file = File::open(filename)?;
+	let mut ret: usize = 0;
+	for _ in io::BufReader::new(file).split(b' ') {
+		ret += 1;
+	}
+	Ok(ret)
+}
+
+fn count_lines(filename: &String) -> Result<usize, io::Error> {
+	let file = File::open(filename)?;
+	let mut ret: usize = 0;
+	for _ in io::BufReader::new(file).lines() {
+		ret += 1;
+	}
+	Ok(ret)
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -11,5 +37,7 @@ fn main() {
     }
     let filename = &args[1];
     // Your code here :)
-
+	println!("{}", count_characters(&filename).expect("counting chars"));
+	println!("{}", count_words(&filename).expect("counting words"));
+	println!("{}", count_lines(&filename).expect("counting lines"));
 }
