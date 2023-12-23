d8af0c2a6d3ca7adb8e79c44121678aaa3ef697c
KatsuragiCSL <shue12345678@gmail.com>
2023-12-24 02:52:13 +0800

    Stanford CS110L week3 part 1 done

--- Stanford CS110L/cs110l-spr-2020-starter-code-main/week3/inspect-fds/src/main.rs
+++ Stanford CS110L/cs110l-spr-2020-starter-code-main/week3/inspect-fds/src/main.rs
@@ -10,11 +10,21 @@ fn main() {
         println!("Usage: {} <name or pid of target>", args[0]);
         std::process::exit(1);
     }
-    #[allow(unused)] // TODO: delete this line for Milestone 1
     let target = &args[1];
-
-    // TODO: Milestone 1: Get the target Process using psutils::get_target()
-    unimplemented!();
+	let process = ps_utils::get_target(&target).expect("Error occurs when calling ps or pgrep");
+	match process {
+		Some(proc) => {
+			proc.print();
+			let children = ps_utils::get_child_processes(proc.pid).expect("Cannot get list of child processes.");
+			for child in children {
+				child.print();
+			}
+		},
+		None => {
+			println!("Target \"{}\" did not match any running PIDs or executables", target);
+			std::process::exit(1);
+		}
+	}
 }
 
 #[cfg(test)]
