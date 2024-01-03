1c58e228c67c9c9f752fbe267efae1dbcfc98c00
KatsuragiCSL <shue12345678@gmail.com>
2024-01-04 02:16:15 +0800

    CMU 15-213 datalab done

--- CMU 15-213/datalab-handout/bits.c
+++ CMU 15-213/datalab-handout/bits.c
@@ -1,7 +1,7 @@
 /* 
  * CS:APP Data Lab 
  * 
- * <Please put your name and userid here>
+ * KatsuragiCSL
  * 
  * bits.c - Source file with your solutions to the Lab.
  *          This is the file you will hand in to your instructor.
@@ -140,7 +140,10 @@ NOTES:
  *   Rating: 1
  */
 int bitXor(int x, int y) {
-  return 2;
+  /*xor = ~(a and b) and (a or b)
+		= ~(a and b) and ~(~a and ~b)
+  */
+  return (~(x & y)) & (~(~x & ~y));
 }
 /* 
  * tmin - return minimum two's complement integer 
@@ -149,7 +152,10 @@ int bitXor(int x, int y) {
  *   Rating: 1
  */
 int tmin(void) {
-  return 2;
+  /* tmin = 100...0 */
+  int tmin;
+  tmin = 1 << 31;
+  return tmin;
 }
 //2
 /*
@@ -160,7 +166,13 @@ int tmin(void) {
  *   Rating: 2
  */
 int isTmax(int x) {
-  return 2;
+  /* tmax + 1 = tmin
+	 tmin ^ tmax = -1
+	check whether ((x+1)^x) + 1 = 0
+	exceptions: -1
+    !(~x) = 1 if x = -1 else 0
+  */
+  return !(((x+1)^x) + 1) & !!(~x);
 }
 /* 
  * allOddBits - return 1 if all odd-numbered bits in word set to 1
@@ -170,7 +182,18 @@ int isTmax(int x) {
  *   Rating: 2
  */
 int allOddBits(int x) {
-  return 2;
+  /* construct odd bits mask by shifting
+     even bits mask =  ~ odd bits mask
+     (x & odd bits mask) ^ even bits mask = -1
+  */
+  int oddMask, evenMask;
+  oddMask = 0b10101010;
+  oddMask = (oddMask << 8) | oddMask;
+  oddMask = (oddMask << 16) | oddMask;
+  evenMask = ~oddMask;
+  
+  int minusOne = (oddMask & x) ^ evenMask;
+  return !(minusOne + 1);
 }
 /* 
  * negate - return -x 
@@ -180,7 +203,7 @@ int allOddBits(int x) {
  *   Rating: 2
  */
 int negate(int x) {
-  return 2;
+  return ~x+1;
 }
 //3
 /* 
@@ -193,7 +216,13 @@ int negate(int x) {
  *   Rating: 3
  */
 int isAsciiDigit(int x) {
-  return 2;
+  /* split into 2 parts: x-0x30 >= 0? and 0x39 - x >=0?
+     x-a can be done by x + negate(a)
+     x>=0 can be done by checking the 31st bit: !(x>>31)
+  */
+  int firstPart = !((x + (~(0x30) + 1)) >> 31);
+  int secondPart = !((0x39 + (~x + 1)) >> 31);
+  return firstPart & secondPart;
 }
 /* 
  * conditional - same as x ? y : z 
@@ -203,7 +232,11 @@ int isAsciiDigit(int x) {
  *   Rating: 3
  */
 int conditional(int x, int y, int z) {
-  return 2;
+  /* check if x = 0 => if no then return y & -1 = y else return z & -1 = z
+     it can be done by returning (y & (mask)) ^ (z & (~mask)) where mask = -1 when x != 0 and 0 when x = 0
+  */
+  int mask = ((!!x)<<31)>>31;
+  return (y & mask) ^ (z & (~mask));
 }
 /* 
  * isLessOrEqual - if x <= y  then return 1, else return 0 
@@ -213,7 +246,19 @@ int conditional(int x, int y, int z) {
  *   Rating: 3
  */
 int isLessOrEqual(int x, int y) {
-  return 2;
+  /* equiv. to check y-x >= 0
+     different sign => over/ underflow
+     same sign => OK
+
+     different sign casses:
+     - y >= 0, x < 0 => always true
+     - y < 0, x >= 0 => always false
+  */
+  int sameSignCmp = !( (y + (~x + 1)) >> 31); // 1 iff y-x >=0;
+  int signX = !(x >> 31); // 1 iff x >= 0
+  int signY = !(y >> 31); // 1 iff y >= 0
+  int isSameSign = !(signX ^ signY); // 1 iff same sign
+  return (isSameSign & sameSignCmp) | (!signX & signY);
 }
 //4
 /* 
@@ -225,7 +270,9 @@ int isLessOrEqual(int x, int y) {
  *   Rating: 4 
  */
 int logicalNeg(int x) {
-  return 2;
+  /* negate(x) | x has 1 as its 31st bit when x != 0. Otherwise 0.
+  */
+  return (~((~x + 1) | x) >> 31) & 1;
 }
 /* howManyBits - return the minimum number of bits required to represent x in
  *             two's complement
@@ -240,7 +287,37 @@ int logicalNeg(int x) {
  *  Rating: 4
  */
 int howManyBits(int x) {
-  return 0;
+  /* positive -> add a leading 0
+     negative -> remove duplicated leading 1's
+     e.g. 5 -> 0101 -> 4 bits
+         -5 -> 111...1011 -> 1011 -> 4 bits
+     Hence,
+     positive -> pos of leading 1 + 1
+     negative -> bit flip -> pos of leading 1 + 1
+     To find pos of leading 1:
+     binary search, shifting x to right by 16, 8, 4, 2, 1 and add it to the result if there is 1's.
+  */
+  int signX = x >> 31; // -1 if x < 0, 0 if x >= 0
+  int xNormalized = (x & ~signX) | (~x & signX); // if x is negative, bit flip.
+  
+  int bin16 = (!!(xNormalized >> 16)) << 4; // 16 if higher 16 bits of x is non-zero, 0 otherwise.
+  xNormalized >>= bin16;
+
+  int bin8 = (!!(xNormalized >> 8)) << 3;
+  xNormalized >>= bin8;
+
+  int bin4 = (!!(xNormalized >> 4)) << 2;
+  xNormalized >>= bin4;
+
+  int bin2 = (!!(xNormalized >> 2)) << 1;
+  xNormalized >>= bin2;
+
+  int bin1 = !!(xNormalized >> 1);
+  xNormalized >>= bin1;
+
+  int bin0 = !!xNormalized;
+
+  return bin16 + bin8 + bin4 + bin2 + bin1 + bin0 + 1;
 }
 //float
 /* 
@@ -255,7 +332,30 @@ int howManyBits(int x) {
  *   Rating: 4
  */
 unsigned float_twice(unsigned uf) {
-  return 2;
+  /* if uf is denorms, returns uf (+/-inf * 2 = +/-inf, NaN * 2 = NaN)
+     if uf is norms, need to consider incrementing E when M >= 2
+     no needs for rounding, cuz uf * 2 always ends with 0 and hence rounded to even
+  */
+  unsigned int sign = 0x80000000 & uf;
+  unsigned int exp = 0x7f800000 & uf;
+  unsigned int frac = 0x7fffff & uf;
+
+  if (((~exp) & 0x7f800000) == 0) {
+    return uf;
+  } else {
+    if (exp == 0) { // near 0, only increment exp if frac > 0.5
+      if ((0x400000 & uf) != 0) {
+        exp = 0x800000;
+      }
+      frac = frac << 1;
+    } else { // always increment exp, if the result beyond the rangeand become inf, set frac to 0
+      exp += 0x800000;
+      if (((~exp) & 0x7f800000) == 0) {
+        frac = 0;
+      }
+    }
+  }
+  return sign | exp | frac;
 }
 /* 
  * float_i2f - Return bit-level equivalent of expression (float) x
@@ -267,7 +367,44 @@ unsigned float_twice(unsigned uf) {
  *   Rating: 4
  */
 unsigned float_i2f(int x) {
-  return 2;
+  /* 1. find pos of leftmost 1 (except sign bit), denote as i. exp = (30-i) + 127
+     2. sign = 31st bit (count from right to left, starts from index 0)
+     3. take the bits at pos [[i, i-22]] as frac
+     4. rounding: if [[7, 0]] is greater than 2^7, or if [[7, 0]] = 127 and 8th bit = 1, then increment frac.
+     exceptions: 0 and tmin
+     don't need to consider the case when exp = 0, cuz x is int
+  */
+  unsigned int sign = 0x80000000 & x;
+  unsigned int frac;
+  unsigned int exp;
+
+  if (x == 0) {
+    return x;
+  }
+  if (x == 0x80000000) { // -2^31 -> sign = 1, exp = 31+127=158, frac = 0
+    return sign | (158 << 23) | 0;
+  }
+  // normal cases
+  if (sign) {
+    x = -x;
+  }
+  int i = 30;
+  while (!(x >> i)) {
+    i--;
+  }
+  exp = i + 127;
+  x = x << (31-i);
+  frac = (x >> 8) & 0x7fffff; //first shift x so that the leading 1 is at the 31st bit, then ignore the lowest 8 bits
+  // rounding
+  if (((x & 0xff) > 128) || ( ((x & 0xff) == 128) && (frac & 1)) ) {
+    frac += 1;
+  }
+  // check if M >= 2;
+  if (frac >> 23) {
+    exp += 1;
+    frac = frac & 0x7fffff;
+  }
+  return sign | (exp << 23) | frac;
 }
 /* 
  * float_f2i - Return bit-level equivalent of expression (int) f
@@ -282,5 +419,38 @@ unsigned float_i2f(int x) {
  *   Rating: 4
  */
 int float_f2i(unsigned uf) {
-  return 2;
+  /* 1. check denorms: if exp = 111...1, return 0x80000000u.
+     2. check denorms: if exp = 0, result always less than 0 so returns 0.
+     3. norms: E = exp - 127. If E < 0, result < 0 and hence return 0. Else take the highest E bits of frac, followed by attaching a leading 1 (i.e 2^E + (frac << (23-E)))
+        if E >= 31, overflows.
+     4. attach the sign.
+  */
+  
+  unsigned int sign = 0x80000000 & uf;
+  unsigned int exp = 0x7f800000 & uf;
+  unsigned int frac = 0x7fffff & uf;
+
+  if (exp == 0b11111111) {
+    return 0x80000000u;
+  } else if (exp == 0) {
+    return 0;
+  } else {
+    exp = exp >> 23;
+    if (exp < 127) {
+      return 0;
+    } else {
+      int E = exp - 127;
+      if (E >= 31) {
+        return 0x80000000u;
+      }
+      if (E > 23) { // no need to truncate frac
+        int result = (frac << E) + 1<<E;
+        return sign? -result: result;
+      } else {
+        frac = frac >> (23-E);
+        int result = frac + 1<<E;
+        return sign? -result: result;
+      }
+    }
+  }
 }
